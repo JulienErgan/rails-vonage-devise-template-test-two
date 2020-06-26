ec6c71e4a523b1f7963e6bd40151dc12ba4716bc
Julien Ergan <julien.ergan.code@gmail.com>
2020-06-26 17:55:15 +0900

    from setup to displaying a room wihtout video

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_26_073149) do
+ActiveRecord::Schema.define(version: 2020_06_26_074848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.string "vonage_session_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
